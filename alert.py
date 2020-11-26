486c91cbca4c173bb9daae300a1a348e2e82e768
Euan Morgan <euanmorgan48@gmail.com>
2020-11-26 16:29:00 +0000

    add email

--- alert.py
+++ alert.py
@@ -3,17 +3,50 @@ import os
 import time
 from termcolor import colored
 import winsound
-
+import smtplib
 
 def play():
     winsound.PlaySound("alarm", winsound.SND_ASYNC | winsound.SND_ALIAS )
 os.system('color')
 
-ips = {"dad":"192.168.1.10","mam":"192.168.1.72","me":"192.168.1.86"}
+def sendMail(name):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
+
+    password = ""
+    server.login('euanmorgan48@gmail.com', password)
+
+    subject = "ALERT!! ALERT!!!!"
+
+    body = f"{name} is outside!!!"
+
+    msg = f"Subject: {subject}\n\n{body}"
+
+    server.sendmail(
+        "euanmorgan48@gmail.com",
+        msg
+    )
+    print("Sent email")
+
+    server.quit()
+
+
+a = input("Mam, dad or both?")
+
+if a.lower() == "mam":
+    ips = {"mam":"192.168.1.42"}
+elif a.lower() == "dad":
+    ips = {"dad":"192.168.1.10"}
+else:
+    ips = {"mam":"192.168.1.42","dad":"192.168.1.10"}
+    
+x = True
 
 with open(os.devnull, "wb") as limbo:
 
-            while True:
+            while x:
                 os.system('cls')
                 for (name,ip) in ips.items():
                     
@@ -21,14 +54,20 @@ with open(os.devnull, "wb") as limbo:
                     result=subprocess.Popen(["ping", "-n", "1", "-w", "200", ip],
                             stdout=limbo, stderr=limbo,shell=True).wait()
 
-                    print(result)
+                    
                     print("Pinging {}".format(name))
                     if result:
-                             
                             print(ip, colored('inactive', 'red'))
+                            
+                            
                     else:
+                            
                             print(ip, colored('active', 'green'))
                             play()
+                            sendMail(name)
+                            time.sleep(4)
+                            x = False
+                            break
                     time.sleep(0.2)
 
                 time.sleep(1.5)
